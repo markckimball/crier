c654ac656794e5e1c33665e4cab23d4b9e53e665
Mark Kimball <markckimball@gmail.com>
2020-08-25 14:41:40 -0600

    Committing stuff before a startover.

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module crier
 go 1.15
 
 require (
-	go.opentelemetry.io/otel v0.10.0
-	go.opentelemetry.io/otel/exporters/trace/jaeger v0.10.0
-	go.opentelemetry.io/otel/sdk v0.10.0
+	github.com/jackc/pgx/v4 v4.8.1
+	github.com/stretchr/testify v1.6.1 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
--- main.go
+++ main.go
@@ -1,70 +1,86 @@
 package main
 
 import (
-	"go.opentelemetry.io/otel/api/correlation"
-	"go.opentelemetry.io/otel/api/global"
-	"go.opentelemetry.io/otel/api/kv"
-	"go.opentelemetry.io/otel/api/trace"
-	"go.opentelemetry.io/otel/instrumentation/httptrace"
-	"go.opentelemetry.io/otel/exporters/trace/jaeger"
-	"io"
+	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
-	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"github.com/jackc/pgx/v4/pgxpool"
 )
 
-// initTracer creates a new trace provider instance and registers it as global trace provider.
-func initTracer() func() {
-	// Create and install Jaeger export pipeline
-	flush, err := jaeger.InstallNewPipeline(
-		jaeger.WithCollectorEndpoint("http://localhost:14268/api/traces"),
-		jaeger.WithProcess(jaeger.Process{
-			ServiceName: "trace-demo",
-			Tags: []kv.KeyValue{
-				kv.String("exporter", "jaeger"),
-				kv.Float64("float", 312.23),
-			},
-		}),
-		jaeger.WithSDK(&sdktrace.Config{DefaultSampler: sdktrace.AlwaysSample()}),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return func() {
-		flush()
-	}
-}
-
 func main() {
-	fn := initTracer()
-	defer fn()
-	tr := global.Tracer("example/server")
+	// Set the flags for the logging package to give us the filename in the logs
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		attrs, entries, spanCtx := httptrace.Extract(req.Context(), req)
+	dbPool := getDBConnection(context.Background())
+	defer dbPool.Close()
 
-		req = req.WithContext(correlation.ContextWithMap(req.Context(), correlation.NewMap(correlation.MapUpdate{
-			MultiKV: entries,
-		})))
+	log.Println("starting server...")
+	http.HandleFunc("/", homeHandler(dbPool))
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
 
-		ctx, span := tr.Start(
-			trace.ContextWithRemoteSpanContext(req.Context(), spanCtx),
-			"hello",
-			trace.WithAttributes(attrs...),
-		)
-		defer span.End()
+func homeHandler(db *pgxpool.Pool) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		visitorID := 0
+		//language=sql
+		err := db.QueryRow(r.Context(), "INSERT INTO visitors(user_agent, datetime) VALUES ($1, now()) RETURNING id", r.UserAgent()).Scan(&visitorID)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte("failed to update the database"))
+			log.Printf("[or] db error: %v\n", err)
+			return
+		}
 
-		span.AddEvent(ctx, "handling this...")
-
-		_, _ = io.WriteString(w, "Hello, world!\n")
-	}
-
-	http.HandleFunc("/hello", helloHandler)
-	err := http.ListenAndServe(":8000", nil)
-	if err != nil {
-		panic(err)
+		w.WriteHeader(http.StatusOK)
+		_, _ = fmt.Fprintf(w, `Hello, visitor %d!`, visitorID)
 	}
 }
 
+func getDBConnection(ctx context.Context) *pgxpool.Pool {
+	// Retrieve the database host address
+	host := os.Getenv("DD_DB_HOST")
+	if host == "" {
+		host = "127.0.0.1"
+	}
+
+	const connectionString = "postgres://crier:crier@%s:5432/crier?sslmode=disable"
+
+	// Try connecting to the database a few times before giving up
+	// Retry to connect for a while
+	var dbPool *pgxpool.Pool
+	var err error
+	for i := 1; i < 8; i++ {
+		log.Printf("trying to connect to the db server (attempt %d)...\n", i)
+		dbPool, err = pgxpool.Connect(ctx, fmt.Sprintf(connectionString, host))
+		if err == nil {
+			break
+		}
+		log.Printf("got error: %v\n", err)
+
+		// Sleep a bit before trying again
+		time.Sleep(time.Duration(i*i) * time.Second)
+	}
+
+	// Stop execution if the database was not initialized
+	if dbPool == nil {
+		log.Fatalln("could not connect to the database")
+	}
+
+	// Get a connection from the pool and check if the database connection is active and working
+	db, err := dbPool.Acquire(ctx)
+	if err != nil {
+		log.Fatalf("failed to get connection on startup: %v\n", err)
+	}
+	if err := db.Conn().Ping(ctx); err != nil {
+		log.Fatalln(err)
+	}
+
+	// Add the connection back to the pool
+	db.Release()
+
+	return dbPool
+}
